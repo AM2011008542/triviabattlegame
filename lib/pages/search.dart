2b4c5a47819281ce1fe7014e3e05e7bbf132305b
Aizat Hadri bin Mohd Najib <kl2011008542@student.kuptm.edu.my>
2022-10-18 16:59:00 +0800

    Initial commit

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -17,136 +17,126 @@ class _SearchPage extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: TextField(
-                      onChanged: (val) {
-                        setState(() {
-                          searchString = val.toLowerCase();
-                        });
-                      },
-                      controller: searchController,
-                      decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              searchController.clear();
-
-                              // reset method here
-                              setState(() {
-                                searchString = searchController.text;
-                              });
-                            },
-                          ),
-                          hintText: "Search user here...",
-                          hintStyle: const TextStyle(
-                            fontFamily: "Antra",
-                            color: Colors.blueGrey,
-                          )
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            title: const Text("Search"),
+            centerTitle: true
+          ),
+          resizeToAvoidBottomInset: false,
+          body: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: TextField(
+                        onChanged: (val) {
+                          setState(() {
+                            searchString = val.toLowerCase();
+                          });
+                        },
+                        controller: searchController,
+                        decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.clear),
+                              onPressed: () {
+                                // reset method here
+                                searchController.clear();
+                                setState(() {
+                                  searchString = searchController.text;
+                                });
+                              },
+                            ),
+                            hintText: "Search user here...",
+                            hintStyle: const TextStyle(
+                              fontFamily: "Antra",
+                              color: Colors.blueGrey,
+                            )),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: StreamBuilder<QuerySnapshot>(
-                      stream: (searchString == null || searchString.trim() == "")
-                          ? FirebaseFirestore.instance.collection("users").snapshots()
-                          : FirebaseFirestore.instance.collection("users").where("index",
-                          arrayContains: searchString).snapshots(),
-                      builder: (context, snapshot) {
-                        if(snapshot.hasError) {
-                          return SizedBox(
-                            child: Center(
-                                child: EmptyWidget(
-                                  packageImage: PackageImage.Image_4,
-                                  hideBackgroundAnimation: true,
-                                  title: "We got an error ${snapshot.error}]",
-                                  titleTextStyle: const TextStyle(
-                                    color: Colors.purple,
-                                    fontSize: 20,
-                                  ),
-                                )
-                            ),
-                          );
-                        }
-                        switch(snapshot.connectionState) {
-                          case ConnectionState.waiting :
-                            return SizedBox(
-                              child: Center(
-                                  child: EmptyWidget(
-                                    image: 'assets/no-found.gif',
-                                    hideBackgroundAnimation: true,
-                                    title: "Loading...",
-                                    titleTextStyle: const TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 20,
-                                    ),
-                                  )
-                              ),
-                            );
-                          case ConnectionState.none:
+                    Expanded(
+                      child: StreamBuilder<QuerySnapshot>(
+                        stream: (searchString == null || searchString.trim() == "")
+                            ? FirebaseFirestore.instance
+                            .collection("users")
+                            .snapshots()
+                            : FirebaseFirestore.instance
+                            .collection("users")
+                            .where("index", arrayContains: searchString)
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
                             return SizedBox(
                               child: Center(
                                   child: EmptyWidget(
                                     packageImage: PackageImage.Image_4,
                                     hideBackgroundAnimation: true,
-                                    title: "None...",
+                                    title: "We got an error ${snapshot.error}]",
                                     titleTextStyle: const TextStyle(
                                       color: Colors.purple,
                                       fontSize: 20,
                                     ),
-                                  )
-                              ),
+                                  )),
                             );
-                          case ConnectionState.done:
-                            return SizedBox(
-                              child: Center(
-                                  child: EmptyWidget(
-                                    packageImage: PackageImage.Image_4,
-                                    hideBackgroundAnimation: true,
-                                    title: "Done...",
-                                    titleTextStyle: const TextStyle(
-                                      color: Colors.purple,
-                                      fontSize: 20,
-                                    ),
-                                  )
-                              ),
-                            );
-                          default:
+                          }
+
+                          if (snapshot.connectionState == ConnectionState.active) {
                             return ListView(
-                                children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                                children: snapshot.data!.docs
+                                    .map((DocumentSnapshot document) {
                                   return GestureDetector(
                                     onTap: () {
-                                      Get.to(Navigator.push(context, MaterialPageRoute(builder: (context) => SearchUserProfilePage(
-                                        userID: document["userID"],))),
+                                      Get.to(
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      SearchUserProfilePage(
+                                                        userID: document["userID"],
+                                                      ))),
                                           transition: Transition.downToUp,
-                                          arguments: document.id
-                                      );
+                                          arguments: document.id);
                                     },
                                     child: ListTile(
                                       leading: CircleAvatar(
-                                        backgroundImage: NetworkImage(document["imageUrl"]),
+                                        backgroundImage:
+                                        NetworkImage(document["imageUrl"]),
                                       ),
                                       title: Text(document["userName"]),
                                       // subtitle: Text(document["userName"]),
                                     ),
                                   );
-                                }).toList()
-                            );
-                        }
-                      },
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        )
+                                }).toList());
+                          }
+
+                          // Otherwise, show something whilst waiting for initialization to complete
+                          return SizedBox(
+                            child: Center(
+                                child: EmptyWidget(
+                                  image: 'assets/no-found.gif',
+                                  hideBackgroundAnimation: true,
+                                  title: "Loading...",
+                                  titleTextStyle: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                  ),
+                                )),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+      ),
     );
   }
 }
