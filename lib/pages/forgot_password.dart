3eaa8546caa6835a0a5f96a55b5446628ecec454
Aizat Hadri bin Mohd Najib <kl2011008542@student.kuptm.edu.my>
2022-10-31 14:03:27 +0800

    Initial commit

--- lib/pages/forgot_password.dart
+++ lib/pages/forgot_password.dart
@@ -24,68 +24,56 @@ class _ForgetPasswordPageState extends State<ForgetPasswordPage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text("Forgot Password"),
+          centerTitle: true,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.arrow_back_outlined),
+          ),
+        ),
         backgroundColor: const Color(0xffEEF1F3),
-        body: Column(
-          children: [
-            const PageHeader(),
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(20),),
+        body: SingleChildScrollView(
+          child: Form(
+            key: _forgetPasswordFormKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: size.height * 0.3,
+                  child: Image.asset('assets/Create-a-kahoot.png'),
                 ),
-                child: SingleChildScrollView(
-                  child: Form(
-                    key: _forgetPasswordFormKey,
-                    child: Column(
-                      children: [
-                        const PageHeading(title: 'Forgot password',),
-                        CustomInputField(
-                            labelText: 'Email',
-                            hintText: 'Your email id',
-                            isDense: true,
-                            validator: (email) {
-                              if(email == null || email.isEmpty) {
-                                return 'Email is required!';
-                              }
-                              if(!EmailValidator.validate(email)) {
-                                return 'Please enter a valid email';
-                              }
-                              emailController.text = email;
-                              return null;
-                            }
-                        ),
-                        const SizedBox(height: 20,),
-                        CustomFormButton(innerText: 'Submit', onPressed: _handleForgetPassword,),
-                        const SizedBox(height: 20,),
-                        Container(
-                          alignment: Alignment.center,
-                          child: GestureDetector(
-                            onTap: () => {
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage())),
-                              SystemChrome.setEnabledSystemUIMode(
-                                  SystemUiMode.leanBack
-                              )
-                            },
-                            child: const Text(
-                              'Back to login',
-                              style: TextStyle(
-                                fontSize: 13,
-                                color: Color(0xff939393),
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                const SizedBox(
+                  height: 16,
                 ),
-              ),
+                CustomInputField(
+                    labelText: 'Email',
+                    hintText: 'Your email id',
+                    isDense: true,
+                    validator: (email) {
+                      if(email == null || email.isEmpty) {
+                        return 'Email is required!';
+                      }
+                      if(!EmailValidator.validate(email)) {
+                        return 'Please enter a valid email';
+                      }
+                      emailController.text = email;
+                      return null;
+                    }
+                ),
+                const SizedBox(height: 20,),
+                CustomFormButton(innerText: 'Submit', onPressed: _handleForgetPassword,),
+                const SizedBox(height: 20,),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
